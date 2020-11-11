908dc622a6784f1ba1f492d0a3d982c808e60657
fummicc1 <fumiya.tennis1@gmail.com>
2020-11-11 12:45:20 +0900

    Initial Commit

---
+++ TableViewSegueSample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableViewSegueSample
+//
+//  Created by Fumiya Tanaka on 2020/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
